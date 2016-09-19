802f37d9ca939966ee080577e9395221c61069db
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2016-09-19 10:31:50 -0600

    asdasd
    segundo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,15 +75,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="http://www.so.ucr.ac.cr/ucrfm/sites/default/files/logo-ucrso.png">
+                    <br>
+                    Bienvenido(a) al modulo de mantenimiento
+                    <br>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="Usuarios">Usuarios</a>
+                    <a href="#">Vehiculos</a>
+                    <a href="#">Inventario</a>
+                    <a href="#">faltantes</a>
                 </div>
             </div>
         </div>
